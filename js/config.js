3e0b36ba7d2485f51343367cd0fd766eda555ad6
Jongkil Park <park108@gmail.com>
2018-06-18 13:15:54 +0900

    Test code for cognito with Google ID - 3
    Add region info

--- js/config.js
+++ js/config.js
@@ -1,19 +1,3 @@
-// window._config = {
-//     cognito: {
-//         userPoolId: 'ap-northeast-2_27STOOpXC', // e.g. us-east-2_uXboG5pAb
-//         userPoolClientId: '3u6u4mjhv8k45soigttas5fikm', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-//         region: 'ap-northeast-2' // e.g. us-east-2
-//     },
-//     api: {
-//         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-//     }
-// };
-
-// // Amazon Cognito 인증 공급자를 초기화합니다
-// // AWS.config.region = 'ap-northeast-2'; // 리전
-// // AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-// //     IdentityPoolId: 'ap-northeast-2:d0984c85-59eb-4a30-baed-cac8d1b750c5',
-// // });
 function onSignIn(googleUser) {
 
   var profile = googleUser.getBasicProfile();
@@ -26,6 +10,7 @@ function onSignIn(googleUser) {
   USER.name = profile.getName();
 
   // Add the Google access token to the Cognito credentials login map.
+  AWS.config.region = 'ap-northeast-2'; // 리전
   AWS.config.credentials = new AWS.CognitoIdentityCredentials({
     IdentityPoolId: 'ap-northeast-2:d0984c85-59eb-4a30-baed-cac8d1b750c5',
     Logins: {
