80d1b1ec90ae137b131801f2aeacb0f8991d78bf
Jongkil Park <park108@gmail.com>
2018-09-18 14:46:01 +0900

    Fix bugs
    1. Remove code for twitter login
    2. Get google api library from google server instead local file

--- js/hvtd-auth.js
+++ js/hvtd-auth.js
@@ -1,5 +1,7 @@
 function onSignInGoogle(googleUser) {
 
+	console.log(googleUser);
+
 	let profile = googleUser.getBasicProfile();
 
 	USER.id = profile.getEmail(); // Google email for hvtd ID
@@ -53,124 +55,8 @@ function onSignInGoogle(googleUser) {
 	});
 }
 
-// function onSignInTwitter() {
-
-// 	let timestamp  = Math.floor(new Date().getTime()/1000);
-
-// 	let urlSplit = document.URL.split("/");
-// 	let callbackUrl = urlSplit[0] + "//" + urlSplit[2];
-
-// 	let requestUrl = "https://api.twitter.com/oauth/request_token";
-// 	let method = "POST";
-// 	let signatureMethod = "HMAC-SHA1";
-// 	let nonce = getNonce(32);
-// 	let oAuthVersion = "1.0";
-
-// 	// Make signature
-// 	let paramString = getParamString(callbackUrl, TWITTER_KEYS.CONSUMER_KEY, nonce, signatureMethod, timestamp, TWITTER_KEYS.ACCESS_TOKEN, oAuthVersion);
-// 	let baseString = getSignatureBaseString(method, requestUrl, paramString);
-// 	let signingKey = percentEncode(TWITTER_KEYS.CONSUMER_SECRET) + "&" + percentEncode(TWITTER_KEYS.ACCESS_TOKEN_SECRET);
-// 	let signature = getSignature("SHA-1", "TEXT", "B64", signingKey, baseString);
-
-// 	log("paramString = " + paramString);
-// 	log("baseString = " + baseString);
-// 	log("signingKey = " + signingKey);
-// 	log("signature = " + signature);
-
-// 	// Make authorization header
-// 	let authHeader = "OAuth ";
-// 	let oauth_callback = "oauth_callback=\"" + percentEncode(callbackUrl) + "\"";
-// 	let oauth_consumer_key = "oauth_consumer_key=\"" + TWITTER_KEYS.CONSUMER_KEY + "\"";
-// 	let oauth_nonce = "oauth_nonce=\"" + nonce + "\"";
-// 	let oauth_signature = "oauth_signature=\"" + percentEncode(signature) + "\"";
-// 	let oauth_signature_method = "oauth_signature_method=\"" + signatureMethod + "\"";
-// 	let oauth_timestamp = "oauth_timestamp=\"" + timestamp + "\"";
-// 	let oauth_version = "oauth_version=\"" + oAuthVersion + "\"";
-
-// 	authHeader = authHeader
-// 				+ oauth_callback
-// 				+ ", "
-// 				+ oauth_consumer_key
-// 				+ ", "
-// 				+ oauth_nonce
-// 				+ ", "
-// 				+ oauth_signature
-// 				+ ", "
-// 				+ oauth_signature_method
-// 				+ ", "
-// 				+ oauth_timestamp
-// 				+ ", "
-// 				+ oauth_version;
-
-// 	log("Authorization = " + authHeader);
-
-// 	// Make and send request
-// 	let req = getXMLHttpRequestObject();
-// 	req.open(method, requestUrl, true);
-// 	req.setRequestHeader("Authorization", authHeader);
-
-// 	req.onreadystatechange = function(e) {
-
-// 		if (req.readyState == 4) {
-// 			if(req.status == 200) {
-// 				log(req.responseText);
-// 			}
-// 			else {
-// 				log("ERROR");
-// 			}
-// 		}
-// 	};
-
-// 	let body = {
-// 		"oauth_callback": callbackUrl
-// 	}
-
-// 	req.send(body);
-// }
-
-// function getParamString(callbackUrl, consumerKey, nonce, signatureMethod, timestamp, accessToken, oAuthVersion) {
-
-// 	let str = "oauth_callback=" + percentEncode(callbackUrl)
-// 			+ "&"
-// 			+ "oauth_consumer_key=" + percentEncode(consumerKey)
-// 			+ "&"
-// 			+ "oauth_nonce=" + percentEncode(nonce)
-// 			+ "&"
-// 			+ "oauth_signature_method=" + percentEncode(signatureMethod)
-// 			+ "&"
-// 			+ "oauth_timestamp=" + percentEncode(timestamp)
-// 			+ "&"
-// 			+ "oauth_version=" + percentEncode(oAuthVersion);
-
-// 	return str;
-// }
-
-// function getSignatureBaseString(method, requestUrl, paramString) {
-
-// 	let str = method + "&"
-// 			+ percentEncode(requestUrl) + "&"
-// 			+ percentEncode(paramString);
-
-// 	return str;
-// }
-
-// function getSignature(variant, inputType, outputType, key, text) {
-
-// 	let shaObj = new jsSHA(variant, inputType);
-// 	shaObj.setHMACKey(key, inputType);
-// 	shaObj.update(text);
-// 	return shaObj.getHMAC(outputType);
-// }
-
-// function getNonce(length) {
-// 	const charset = '0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz'
-// 	const result = [];
-// 	window.crypto.getRandomValues(new Uint8Array(length)).forEach(c =>
-// 		result.push(charset[c % charset.length]));
-// 	return result.join('');
-// }
-
 function onSignInFailure(e) {
+	log(e);
 	signOut();
 }
 
